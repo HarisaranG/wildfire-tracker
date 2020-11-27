61c89f0b012ec4371c04993b34f7a8980cab1c84
HarisaranG <harithiru2018@gmail.com>
2020-11-27 14:43:44 +0530

    Wildfire Tracker v1.0

---
+++ src/components/Map.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import GoogleMapReact from 'google-map-react';
+import LocationMarker from './LocationMarker';
+import LocationInfoBox from './LocationInfoBox';
+
+const Map = ({ eventData, center, zoom }) => {
+    const markers = eventData.map(ev => {
+        if (ev.categories[0].id === 8){
+            return <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} 
+            onClick={() => setLocationInfo({ id: ev.id, title: ev.title })}/>;
+        }
+        return null;
+    });
+    const [locationInfo, setLocationInfo] = useState();
+    return (
+        <div className="map">
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyBVG1AFgOTC7nAqOFY3Tmb1KAUqoWRzuUo' }}
+                defaultCenter={ center }
+                defaultZoom={ zoom }
+            >
+                {markers}
+            </GoogleMapReact>
+            { locationInfo ? <LocationInfoBox info={locationInfo} /> : null }
+        </div>
+    )
+}
+
+Map.defaultProps = {
+    center: {
+        lat: 13.0033,
+        lng: 80.2550
+    },
+    zoom: 6
+}
+
+export default Map;
