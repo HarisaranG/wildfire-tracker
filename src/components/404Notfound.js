dd7de94b369a9b845469c99a6a1c77855064c8e1
HarisaranG <harithiru2018@gmail.com>
2020-12-11 17:38:20 +0530

    Final Commit

--- src/components/404Notfound.js
+++ src/components/404Notfound.js
@@ -5,7 +5,7 @@ const FourOhFour = () => {
         <div id="not-found">
             <h1 id="h404">404</h1>
             <p id="p404">Oops! Something is wrong.</p>
-            <a class="button" href="#"><i class="icon-home"></i> Go back in initial page, is better.</a>
+            {/* <a class="button" href="#"><i class="icon-home"></i> Go back in initial page, is better.</a> */}
         </div>
     )
 }
